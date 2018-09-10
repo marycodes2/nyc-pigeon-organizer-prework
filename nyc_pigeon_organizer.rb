c09ac9045059c81f926f77d02d3cbd39c2a13b90
Mary  Beliveau <mary.e.beliveau@gmail.com>
2018-09-10 22:21:27 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,48 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_dict = {}
+  data.each do |attribute_name, attribute_data|
+    attribute_data.each do |attribute_description, pigeon_name|
+      pigeon_name.each do |name|
+        if new_pigeon_dict.keys.empty? == true
+          new_pigeon_dict[name] = ""
+        elsif new_pigeon_dict.keys.include? name == false
+          puts "hi"
+          second_layer_dict = {}
+          second_layer_dict[attribute_name] = attribute_description.to_s
+          new_pigeon_dict[name] = second_layer_dict
+        elsif new_pigeon_dict.keys.include? name == true
+          second_layer_dict = {}
+          second_layer_dict[attribute_name] = attribute_description.to_s
+          new_pigeon_dict[name] = second_layer_dict
+          puts new_pigeon_dict
+        else
+          puts "whoops"
+        end 
+
+      end 
+    end 
+  end
+  new_pigeon_dict
 end
+  
+
+puts nyc_pigeon_organizer(pigeon_data)
